e9257e921c7e74d9e95a806fe876f8c89ec60caf
Takahiro Kamekura <takahiro.kamekura@mmmcorp.co.jp>
2024-04-25 19:35:54 +0900

    fix to separate stage into npm-install and build

--- lib/code-pipeline-stack.ts
+++ lib/code-pipeline-stack.ts
@@ -53,17 +53,46 @@ export class CodePipelineStack extends Stack {
     });
 
     // ビルドプロジェクトの作成
-    const project = new PipelineProject(this, "PipelineProject", {
+    const npmInstallProjectSg = new SecurityGroup(this, "PipelineProjectSg", {
+      vpc,
+    });
+    npmInstallProjectSg.connections.allowTo(vpceSg, Port.tcp(443));
+    const npmInstallProject = new PipelineProject(this, "NpmInstallPipelineProject", {
+      vpc: vpc,
+      subnetSelection: vpc.selectSubnets({ subnetGroupName: SUBNET_GROUP_NAME }),
+      securityGroups: [npmInstallProjectSg],
+      environment: {
+        buildImage: LinuxBuildImage.AMAZON_LINUX_2_5,
+        computeType: ComputeType.LARGE,
+      },
       buildSpec: BuildSpec.fromObjectToYaml({
         version: 0.2,
         phases: {
           // CodeArtifactへのログイン
           install: {
-            commands: ["pip3 install awscli --upgrade --user", `aws codeartifact login --tool npm --domain my-domain --repository my-repo`],
+            commands: [`aws codeartifact login --tool npm --domain my-domain --repository my-repo`],
           },
+          // npm install
+          build: {
+            commands: ["cd sample-app", "npm install"],
+          },
+        },
+        artifacts: {
+          files: ["**/*"],
+        },
+      }),
+    });
+    const buildProject = new PipelineProject(this, "BuildPipelineProject", {
+      buildSpec: BuildSpec.fromObjectToYaml({
+        version: 0.2,
+        environment: {
+          buildImage: LinuxBuildImage.AMAZON_LINUX_2_5,
+          computeType: ComputeType.LARGE,
+        },
+        phases: {
           // ビルド
           build: {
-            commands: ["cd sample-app", "npm install", "npm run build"],
+            commands: ["cd sample-app", "npm run build"],
           },
           // ビルドイメージのECRへのPush
           post_build: {
@@ -78,14 +107,14 @@ export class CodePipelineStack extends Stack {
     });
 
     // CodeArtifactへのログインに必要なポリシーの設定
-    project.addToRolePolicy(
+    npmInstallProject.addToRolePolicy(
       new PolicyStatement({
         actions: ["codeartifact:GetAuthorizationToken", "codeartifact:GetRepositoryEndpoint", "codeartifact:ReadFromRepository"],
         effect: Effect.ALLOW,
         resources: ["*"],
       })
     );
-    project.addToRolePolicy(
+    npmInstallProject.addToRolePolicy(
       new PolicyStatement({
         actions: ["sts:GetServiceBearerToken"],
         effect: Effect.ALLOW,
@@ -98,7 +127,7 @@ export class CodePipelineStack extends Stack {
       })
     );
     // ECRへのPushに必要なポリシーの設定
-    project.addToRolePolicy(
+    buildProject.addToRolePolicy(
       new PolicyStatement({
         actions: [
           "ecr:BatchCheckLayerAvailability",
@@ -108,15 +137,14 @@ export class CodePipelineStack extends Stack {
           "ecr:PutImage",
           "ecr:UploadLayerPart",
         ],
-        resources: [
-          "*"
-        ],
-        effect: Effect.ALLOW
+        resources: ["*"],
+        effect: Effect.ALLOW,
       })
     );
 
     // パイプラインの作成
     const srcArtifact = new Artifact("src");
+    const npmInstallArtifact = new Artifact("npm");
     const buildArtifact = new Artifact("build")
 
     const pipeline = new Pipeline(this, "Pipeline", {
@@ -133,12 +161,23 @@ export class CodePipelineStack extends Stack {
       ],
     });
     pipeline.addStage({
+      stageName: "NpmInstallStage",
+      actions: [
+        new CodeBuildAction({
+          actionName: "NpmInstallAction",
+          input: srcArtifact,
+          project: npmInstallProject,
+          outputs: [npmInstallArtifact],
+        }),
+      ],
+    });
+    pipeline.addStage({
       stageName: "BuildStage",
       actions: [
         new CodeBuildAction({
           actionName: "BuildAction",
-          input: srcArtifact,
-          project: project,
+          input: npmInstallArtifact,
+          project: buildProject,
           outputs: [buildArtifact],
         }),
       ],
